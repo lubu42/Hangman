7e3dcdfdae92384c83125ca83fafdfe9e030fc21
lubu42 <michaeljhogan42@gmail.com>
2024-04-08 19:43:38 +0000

    Still adding hidden character functionality

--- hangman.py
+++ hangman.py
@@ -1,23 +1,28 @@
 answer = "apple"
 hangman = len(answer)
+solution = answer
 
 
 while True:
-	guess = input("Please input your letter as a guess: ")
-	
-	for i in answer:
-		if i == guess:
-			answer = answer.replace(i, '', 1)
-			if answer == "":
-				print("You have won!")
-				exit()
-			print("'" + guess + "'" + " is a correct letter!")
-			break
-		else:
-			hangman = (hangman - 1)
-			if hangman == 0:
-				print("You Lose!")
-				exit()
-			else:
-				print(("You have ") + str(hangman) + (" tries left"))
-				break
+    guess = input("Please input your letter as a guess: ")
+
+    for i in answer:
+        if i == guess:
+            for i in solution:
+                if i != guess:
+                    solution.replace(i, "x")
+                    print(solution)
+            answer = answer.replace(i, '', 1)
+            if answer == "":
+                print("You have won!")
+                exit()
+            print("'" + guess + "'" + " is a correct letter!")
+            break
+        else:
+            hangman = (hangman - 1)
+            if hangman == 0:
+                print("You Lose!")
+                exit()
+            else:
+                print(("You have ") + str(hangman) + (" tries left"))
+                break
